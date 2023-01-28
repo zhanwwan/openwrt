50074d8c7f3ab47ffe9605bb1d9ad4eb6d6dc03d
Wang Zhanwu <wangzhanwu@espressif.com>
2023-01-28 15:46:38 +0800

    Add support linux 6.1 on rockchip

--- target/linux/rockchip/image/armv8.mk
+++ target/linux/rockchip/image/armv8.mk
@@ -5,6 +5,25 @@
 # FIT will be loaded at 0x02080000. Leave 16M for that, align it to 2M and load the kernel after it.
 KERNEL_LOADADDR := 0x03200000
 
+define Device/firefly_roc-cc
+  DEVICE_VENDOR := Firefly
+  DEVICE_MODEL := ROC-RK3328-CC
+  SOC := rk3328
+  UBOOT_DEVICE_NAME := roc-cc-rk3328
+  SUPPORTED_DEVICES := firefly,roc-rk3328-cc
+  IMAGE/sysupgrade.img.gz := boot-common | boot-script nanopi-r2s | pine64-img | gzip | append-metadata
+endef
+TARGET_DEVICES += firefly_roc-cc
+
+define Device/friendlyarm_nanopi-neo3
+  DEVICE_VENDOR := FriendlyARM
+  DEVICE_MODEL := NanoPi NEO3
+  SOC := rk3328
+  UBOOT_DEVICE_NAME := nanopi-r2s-rk3328
+  IMAGE/sysupgrade.img.gz := boot-common | boot-script nanopi-r2s | pine64-img | gzip | append-metadata
+endef
+TARGET_DEVICES += friendlyarm_nanopi-neo3
+
 define Device/friendlyarm_nanopi-r2s
   DEVICE_VENDOR := FriendlyARM
   DEVICE_MODEL := NanoPi R2S
@@ -26,6 +45,16 @@ define Device/friendlyarm_nanopi-r4s
 endef
 TARGET_DEVICES += friendlyarm_nanopi-r4s
 
+define Device/friendlyelec_nanopi-r5s
+  DEVICE_VENDOR := Friendlyelec
+  DEVICE_MODEL := NanoPi R5S
+  SOC := rk3568
+  UBOOT_DEVICE_NAME := nanopi-r5s-rk3568
+  IMAGE/sysupgrade.img.gz := boot-common | boot-script nanopi-r5s | pine64-img | gzip | append-metadata
+  DEVICE_PACKAGES := kmod-r8169
+endef
+TARGET_DEVICES += friendlyelec_nanopi-r5s
+
 define Device/pine64_rockpro64
   DEVICE_VENDOR := Pine64
   DEVICE_MODEL := RockPro64
@@ -35,12 +64,47 @@ define Device/pine64_rockpro64
 endef
 TARGET_DEVICES += pine64_rockpro64
 
-define Device/radxa_rock-pi-4a
+define Device/pine64_quartz64-a
+  DEVICE_VENDOR := Pine64
+  DEVICE_MODEL := QUARTZ64
+  SOC := rk3566
+  SUPPORTED_DEVICES := pine64,quartz64-a
+  UBOOT_DEVICE_NAME := quartz64-a-rk3566
+  IMAGE/sysupgrade.img.gz := boot-common | boot-script quartz64-a | pine64-img | gzip | append-metadata
+endef
+TARGET_DEVICES += pine64_quartz64-a
+
+define Device/radxa_rock-3a
   DEVICE_VENDOR := Radxa
-  DEVICE_MODEL := ROCK Pi 4A
+  DEVICE_MODEL := ROCK3 Model A
+  SOC := rk3568
+  SUPPORTED_DEVICES := radxa,rock3a
+  UBOOT_DEVICE_NAME := rock-3a-rk3568
+  IMAGE/sysupgrade.img.gz := boot-common | boot-script rock-3a | pine64-img | gzip | append-metadata
+endef
+TARGET_DEVICES += radxa_rock-3a
+
+define Device/radxa_rock-pi-4
+  DEVICE_VENDOR := Radxa
+  DEVICE_MODEL := ROCK Pi 4
+  DEVICE_DTS := rockchip/rk3399-rock-pi-4b
   SOC := rk3399
-  SUPPORTED_DEVICES := radxa,rockpi4a radxa,rockpi4
+  SUPPORTED_DEVICES := radxa,rockpi4b
   UBOOT_DEVICE_NAME := rock-pi-4-rk3399
   IMAGE/sysupgrade.img.gz := boot-common | boot-script | pine64-img | gzip | append-metadata
+  DEVICE_PACKAGES := \
+	brcmfmac-firmware-43456-sdio \
+	brcmfmac-firmware-43456-sdio-rockpi-4 \
+	kmod-brcmfmac wpad-basic-wolfssl
 endef
-TARGET_DEVICES += radxa_rock-pi-4a
+TARGET_DEVICES += radxa_rock-pi-4
+
+define Device/rockchip_bpi-r2-pro
+  DEVICE_VENDOR := Rockchip
+  DEVICE_MODEL := Bananapi-R2 Pro
+  SOC := rk3568
+  SUPPORTED_DEVICES := rockchip,rk3568-bpi-r2pro
+  UBOOT_DEVICE_NAME := bpi-r2-pro-rk3568
+  IMAGE/sysupgrade.img.gz := boot-common | boot-script quartz64-a | pine64-img | gzip | append-metadata
+endef
+TARGET_DEVICES += rockchip_bpi-r2-pro
